79d99dd00f85a92f4d7e6b26fa929fe76e0963ec
ppby <salswnss@gmail.com>
2020-07-07 17:20:08 +0900

    Setting 'App에서 상태관리하기'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: '리액트 기초 알아보기',
+      checked: true,
+    },
+    {
+      id: 2,
+      text: '리액트 마스터',
+      checked: false,
+    },
+    {
+      id: 3,
+      text: 'Awsome한 회사 다니기',
+      checked: false,
+    },
+  ]);
+
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 }
