b97d73fb202cb5883ea022b061f02c622d69c669
ppby <salswnss@gmail.com>
2020-07-07 22:17:34 +0900

    Add TodoList onSubmit

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { TiPlusOutline } from 'react-icons/ti';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue('');
+
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
   return (
-    <form action="" className="TodoInsert">
-      <input type="text" placeholder="할 일을 입력하세요" />
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <TiPlusOutline />
       </button>
