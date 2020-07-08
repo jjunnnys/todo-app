4e69665308594f33b5d36731456b42709c0fd17a
ppby <salswnss@gmail.com>
2020-07-08 12:28:44 +0900

    Modify useState의 함수형 업데이트

--- src/App.js
+++ src/App.js
@@ -27,30 +27,25 @@ function App() {
         text,
         checked: false,
       };
-      setTodos(todos.concat(todo));
+      setTodos((todos) => todos.concat(todo));
       nextId.current += 1;
     },
-    [todos],
+    [], // 함수형 업데이트를 쓰면 useCallback의 두 번째 파라미터의 배열에 값을 안 넣을 수 있음
   );
 
-  const onRemove = useCallback(
-    (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id)); // TodoList에서 props로 받아온 id 값이 현재 선택한 id값과 다른 것들만 따로 배열을 만듬
-    },
-    [todos],
-  );
+  const onRemove = useCallback((id) => {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id)); // TodoList에서 props로 받아온 id 값이 현재 선택한 id값과 다른 것들만 따로 배열을 만듬
+  }, []);
 
-  const onToggle = useCallback(
-    (id) => {
-      setTodos(
-        // map 함수에선 {} 대신 ()를 쓴다....?
+  const onToggle = useCallback((id) => {
+    setTodos(
+      // map 함수에선 {} 대신 ()를 쓴다....?
+      (todos) =>
         todos.map((todo) =>
           todo.id === id ? { ...todo, checked: !todo.checked } : todo,
         ),
-      );
-    },
-    [todos],
-  );
+    );
+  }, []);
 
   return (
     <TodoTemplate>
